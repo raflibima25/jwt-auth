070b1bbb7552763077c785d34fd20f7bce993e73
Rafli Bima Pratandra <raflibima1106@gmail.com>
2023-02-06 08:49:52 +0700

    add Users controller

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import db from "./config/Database.js";
+// import Users from "./models/UserModel.js";
 
 const app = express();
 
 try {
 	await db.authenticate();
 	console.log("Database Connected...");
+	// await Users.sync();
 } catch (error) {
 	console.log(error);
 }
