80089c9b48b8fd63e6e1d8f32d88ceca71a5e35e
Rafli Bima Pratandra <raflibima1106@gmail.com>
2023-02-06 12:13:21 +0700

    add refresh token func

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import db from "./config/Database.js";
 // import Users from "./models/UserModel.js";
 import router from "./routes/index.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
@@ -15,6 +16,7 @@ try {
 	console.log(error);
 }
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
