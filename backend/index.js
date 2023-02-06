c705e26bc7f2de6c0a883137920cd4d8ea2b13cc
Rafli Bima Pratandra <raflibima1106@gmail.com>
2023-02-06 08:20:45 +0700

    add database connection

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,13 @@
 import express from "express";
+import db from "./config/Database.js";
 
 const app = express();
+
+try {
+	await db.authenticate();
+	console.log("Database Connected...");
+} catch (error) {
+	console.log(error);
+}
 
 app.listen(5000, () => console.log("Server running at port 5000"));
