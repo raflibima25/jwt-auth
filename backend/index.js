657ab6d84fe8657f9b9d440955c581a789ef8d0d
Rafli Bima Pratandra <raflibima1106@gmail.com>
2023-02-06 08:58:04 +0700

    add router

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/Database.js";
 // import Users from "./models/UserModel.js";
+import router from "./routes/index.js";
 
 const app = express();
 
@@ -11,5 +12,8 @@ try {
 } catch (error) {
 	console.log(error);
 }
+
+app.use(express.json());
+app.use(router);
 
 app.listen(5000, () => console.log("Server running at port 5000"));
