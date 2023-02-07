1bc50711044510526a4c7cc9cec148cec16e941c
Rafli Bima Pratandra <raflibima1106@gmail.com>
2023-02-07 13:54:06 +0700

    add Register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
 	return (
-		<div>
-			<Login />
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Login />} />
+				<Route path='/register' element={<Register />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
