a7bca2cef3055500e84026a4f59117fc24e9e2fa
Rafli Bima Pratandra <raflibima1106@gmail.com>
2023-02-07 12:39:44 +0700

    --end backend, add cors so that API can be accessed outside the domain

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import db from "./config/Database.js";
 // import Users from "./models/UserModel.js";
+import cors from "cors";
 import router from "./routes/index.js";
 import cookieParser from "cookie-parser";
 
@@ -16,6 +17,7 @@ try {
 	console.log(error);
 }
 
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
