9c1a2f1f97d10a832b34a1aee8851b5565b174cd
Rafli Bima Pratandra <raflibima1106@gmail.com>
2023-02-09 14:36:26 +0700

    fix error message in Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
 						<img src='https://bulma.io/images/bulma-logo.png' alt='bulma' width='112' height='28' />
 					</a>
 
-					<a role='button' className='navbar-burger burger' aria-label='menu' aria-expanded='false' data-target='navbarBasicExample'>
+					<a href='/' role='button' className='navbar-burger burger' aria-label='menu' aria-expanded='false' data-target='navbarBasicExample'>
 						<span aria-hidden='true'></span>
 						<span aria-hidden='true'></span>
 						<span aria-hidden='true'></span>
@@ -18,13 +18,15 @@ const Navbar = () => {
 
 				<div id='navbarBasicExample' className='navbar-menu'>
 					<div className='navbar-start'>
-						<a className='navbar-item'>Home</a>
+						<a href='/' className='navbar-item'>
+							Home
+						</a>
 					</div>
 
 					<div className='navbar-end'>
 						<div className='navbar-item'>
 							<div className='buttons'>
-								<a className='button is-light'>Log Out</a>
+								<button className='button is-light'>Log Out</button>
 							</div>
 						</div>
 					</div>
