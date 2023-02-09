1a6594409355f3f9f46964adde76bac5748ef23c
Rafli Bima Pratandra <raflibima1106@gmail.com>
2023-02-09 15:04:19 +0700

    add func where get users data can get token too without refresh page

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -30,11 +30,43 @@ const Dashboard = () => {
 		}
 	};
 
+	const axiosJWT = axios.create();
+
+	axiosJWT.interceptors.request.use(
+		async (config) => {
+			const currentDate = new Date();
+			if (expire * 1000 < currentDate.getTime()) {
+				const response = await axios.get("http://localhost:5000/token");
+				config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+				setToken(response.data.accessToken);
+				const decoded = jwt_decode(response.data.accessToken);
+				setName(decoded.name);
+				setExpire(decoded.exp);
+			}
+			return config;
+		},
+		(error) => {
+			return Promise.reject(error);
+		}
+	);
+
+	const getUsers = async () => {
+		const response = await axiosJWT.get("http://localhost:5000/users", {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
+		console.log(response.data);
+	};
+
 	return (
 		<div>
 			<Navbar />
 			<div className='container mt-5'>
 				<h1>Welkom Bek: {name}</h1>
+				<button onClick={getUsers} className='button is-info mt-2'>
+					Get Users
+				</button>
 			</div>
 		</div>
 	);
