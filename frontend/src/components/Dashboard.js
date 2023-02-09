8c6d6fae91c6a8c49a08c32f3e2000052e4ee0f6
Rafli Bima Pratandra <raflibima1106@gmail.com>
2023-02-09 14:34:32 +0700

    add func refreshToken for get data

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,12 +1,32 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 
 const Dashboard = () => {
+	const [name, setName] = useState("");
+	const [token, setToken] = useState("");
+
+	useEffect(() => {
+		refreshToken();
+	}, []);
+
+	const refreshToken = async () => {
+		try {
+			const response = await axios.get("http://localhost:5000/token");
+			setToken(response.data.accessToken);
+			const decoded = jwt_decode(response.data.accessToken);
+			setName(decoded.name);
+		} catch (error) {}
+	};
+
 	return (
 		<div>
 			<Navbar />
 			<div className='container mt-5'>
-				<h1>Welkom Bek: </h1>
+				<h1>Welkom Bek: {name}</h1>
 			</div>
 		</div>
 	);
